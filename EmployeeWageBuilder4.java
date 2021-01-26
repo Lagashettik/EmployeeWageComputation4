9125ce4ecbb1bf5755a662dd4929a9e42c634319
Lagashettik <Lagashettik@gmail.com>
2021-01-26 13:08:01 +0530

    Added: Employee Attendance using if-else

--- EmployeeWageBuilder4.java
+++ EmployeeWageBuilder4.java
@@ -5,7 +5,16 @@ public class EmployeeWageBuilder4 {
 		System.out.println("Welcome to Employee Wage Computation");
 		}
 
+	void Attendance(){
+		int empCheck=(int) Math.floor(Math.random() * 10) % 2;
+		if(empCheck == 0)
+			System.out.println("Employee is Absent");
+		else
+			System.out.println("Employee is Present");
+	}
+
 	public static void main(String args[]){
-	EmployeeWageBuilder4 emp= new EmployeeWageBuilder4();
+	EmployeeWageBuilder4 Emp= new EmployeeWageBuilder4();
+	Emp.Attendance();
 	}
 }
