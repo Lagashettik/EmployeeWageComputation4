ab6c74b99b455a81eed6ae515f55c91c94c6785f
Lagashettik <Lagashettik@gmail.com>
2021-01-26 13:32:13 +0530

    Added: Part Time wage using if-else

--- EmployeeWageBuilder4.java
+++ EmployeeWageBuilder4.java
@@ -9,22 +9,32 @@ public class EmployeeWageBuilder4 {
 		}
 
 	void Attendance(){
-		int empCheck=(int) Math.floor(Math.random() * 10) % 2;
+		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+		// empCheck=0 Employee is Absent
+		// empCheck=1 Employee is Present
+		// empCheck=2 Employee is Part Time
+
 		if(empCheck == 0){
 			System.out.println("Employee is Absent");
 			NoOfHours=0;
 			}
-		else{
+		else if(empCheck == 1){
 			System.out.println("Employee is Present");
 			NoOfHours=8;
+			}
+		else{
+			System.out.println("Employee is Part Time");
+			NoOfHours=4;
 			}
 	}
 
 	void DailyWage(){
-	Attendance();
-	int Payment = WagePerHour * NoOfHours;
-	System.out.println("Daily wage is "+Payment);
+		Attendance();
+
+		int Payment = WagePerHour * NoOfHours;
+		System.out.println("Daily wage is "+Payment);
 	}
+
 	public static void main(String args[]){
 	EmployeeWageBuilder4 Emp= new EmployeeWageBuilder4();
 	Emp.DailyWage();
