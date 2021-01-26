7681ee0293a5eafc431bd505af3bfc8d8dfd42b2
Lagashettik <Lagashettik@gmail.com>
2021-01-26 13:00:17 +0530

    Welcome Message

---
+++ EmployeeWageBuilder4.java
@@ -0,0 +1,11 @@
+
+public class EmployeeWageBuilder4 {
+
+	EmployeeWageBuilder4() {
+		System.out.println("Welcome to Employee Wage Computation");
+		}
+
+	public static void main(String args[]){
+	EmployeeWageBuilder4 emp= new EmployeeWageBuilder4();
+	}
+}
