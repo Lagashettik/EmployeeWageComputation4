d1fa841abd917c6f947355c1864ac17df9a90b11
Lagashettik <Lagashettik@gmail.com>
2021-01-26 15:05:25 +0530

    Employee wage multiple company using class method

--- EmployeeWageBuilder4.java
+++ EmployeeWageBuilder4.java
@@ -1,19 +1,35 @@
-
+import java.util.*;
 public class EmployeeWageBuilder4 {
 
-	int WagePerHour=20;
+	int WagePerHour;
 	int NoOfHours=0;
-	int month=20;
-	int day=1;
-	int MaxHours=100;
+	int MaxDays;
+	int Day=1;
+	int MaxHours;
+	String company;
 
-	EmployeeWageBuilder4() {
+	EmployeeWageBuilder4(){}
+	EmployeeWageBuilder4(String s) {
 		System.out.println("Welcome to Employee Wage Computation");
 		}
 
-	void DailyWage(){
+	void InDetails(){
+	Scanner Sc = new Scanner(System.in);
+	System.out.println("Enter Following Details");
+	System.out.println("Company Name");
+	company=Sc.nextLine();
+	System.out.println("Wage per Hour");
+	WagePerHour=Sc.nextInt();
+	System.out.println("Maximum Hours");
+	MaxHours=Sc.nextInt();
+	System.out.println("Maximum Days");
+	MaxDays=Sc.nextInt();
 
-		while(day <= month){
+	}
+
+	void WageCalc(){
+
+		while(Day <= MaxDays){
 			int empCheck=(int) Math.floor(Math.random() * 10) % 3;
 			// empCheck=0 Employee is Absent
 			// empCheck=1 Employee is Present
@@ -27,21 +43,24 @@ public class EmployeeWageBuilder4 {
 				NoOfHours+=4;
 				break;
 			}
-		day++;
+		Day++;
 		if(MaxHours <= NoOfHours)
 				break;
 		}
 	}
 
 	void Wage(){
-		DailyWage();
+		InDetails();
+		WageCalc();
 
 		int Payment = WagePerHour * NoOfHours;
-		System.out.println("Employee wage is "+Payment);
+		System.out.println("Employee total wage is "+Payment);
 	}
 
 	public static void main(String args[]){
-	EmployeeWageBuilder4 Emp= new EmployeeWageBuilder4();
-	Emp.Wage();
+	EmployeeWageBuilder4 Emp1= new EmployeeWageBuilder4("String");
+	EmployeeWageBuilder4 Emp2= new EmployeeWageBuilder4();
+	Emp1.Wage();
+	Emp2.Wage();
 	}
 }
